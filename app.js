08640ea66f78385bd0d976f5e23f5563c401261c
AMohorev <mrartem61@gmail.com>
2021-05-28 16:40:57 +0300

    Fixes

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.use((error, req, res, next) => {
     if (req.headersSent) {
         return next(error);
     }
+    console.log(error);
     res.status(error.code || 500);
     res.json({ message: error.message || "Unknown error" });
 });
